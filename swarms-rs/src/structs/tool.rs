bdd2f5a9fca0d3d69ee66927cf42c64dee5bd4c4
M4n5ter <m4n5terrr@gmail.com>
2025-04-15 08:43:37 +0800

    feat(mcp): integrate MCP tools into SwarmsAgent
    Add support for MCP tools by integrating the rmcp library into the SwarmsAgent. This includes adding MCPTool struct, enabling SSE and stdio transport for MCP servers, and handling multiple tool calls in the agent's response processing. The integration allows the agent to utilize external tools provided by MCP servers, enhancing its functionality and flexibility.

--- swarms-rs/src/structs/tool.rs
+++ swarms-rs/src/structs/tool.rs
@@ -1,6 +1,12 @@
 use futures::future::BoxFuture;
+use rmcp::{
+    RoleClient,
+    model::CallToolRequestParam,
+    service::{DynService, RunningService},
+};
 use serde::{Deserialize, Serialize};
-use std::future::Future;
+use std::{future::Future, ops::Deref, sync::Arc};
+use thiserror::Error;
 
 use crate::llm::request::ToolDefinition;
 
@@ -63,5 +69,128 @@ impl<T: Tool> ToolDyn for T {
                 Err(e) => Err(ToolError::JsonError(e)),
             }
         })
+    }
+}
+
+pub struct MCPTool {
+    tool: rmcp::model::Tool,
+    client: Arc<RunningService<RoleClient, Box<dyn DynService<RoleClient>>>>,
+}
+
+impl MCPTool {
+    pub fn from_server(
+        tool: rmcp::model::Tool,
+        client: Arc<RunningService<RoleClient, Box<dyn DynService<RoleClient>>>>,
+    ) -> Self {
+        Self { tool, client }
+    }
+}
+
+impl Tool for MCPTool {
+    type Error = ToolError;
+
+    type Args = serde_json::Map<String, serde_json::Value>;
+
+    type Output = String;
+
+    // We don't need NAME for MCPTool
+    const NAME: &'static str = "";
+
+    fn name(&self) -> String {
+        self.tool.name.to_string()
+    }
+
+    fn definition(&self) -> ToolDefinition {
+        ToolDefinition::from(&self.tool)
+    }
+
+    async fn call(
+        &self,
+        args: serde_json::Map<String, serde_json::Value>,
+    ) -> Result<Self::Output, Self::Error> {
+        let result = self
+            .client
+            .call_tool(CallToolRequestParam {
+                name: Tool::name(self).into(),
+                arguments: Some(args),
+            })
+            .await
+            .map_err(|e| MCPToolError(format!("MCP tool call failed: {e}")))?;
+
+        if result.is_error.unwrap_or(false) {
+            return Err(ToolError::from(MCPToolError(format!(
+                "MCP tool call failed, content: {:?}",
+                result.content
+            ))));
+        }
+
+        Ok(result
+            .content
+            .into_iter()
+            .map(|content| match content.raw {
+                rmcp::model::RawContent::Text(raw_text_content) => raw_text_content.text,
+                rmcp::model::RawContent::Image(raw_image_content) => format!(
+                    "data:{};base64,{}",
+                    raw_image_content.mime_type, raw_image_content.data
+                ),
+                rmcp::model::RawContent::Resource(rmcp::model::RawEmbeddedResource {
+                    resource,
+                }) => match resource {
+                    rmcp::model::ResourceContents::TextResourceContents {
+                        uri,
+                        mime_type,
+                        text,
+                    } => format!(
+                        "[URI]:{}\n{}[TEXT]:{}",
+                        uri,
+                        mime_type.map_or("".to_owned(), |m| format!("[MIME]:{}\n", m)),
+                        text
+                    ),
+                    rmcp::model::ResourceContents::BlobResourceContents {
+                        uri,
+                        mime_type,
+                        blob,
+                    } => format!(
+                        "[URI]:{}\n{}[BLOB]:{}",
+                        uri,
+                        mime_type.map_or("".to_owned(), |mime| format!("[MIME]:{mime}\n")),
+                        blob
+                    ),
+                },
+                rmcp::model::RawContent::Audio(annotated) => format!(
+                    "data:{};base64,{}",
+                    annotated.raw.mime_type, annotated.raw.data
+                ),
+            })
+            .collect::<Vec<_>>()
+            .join(""))
+    }
+}
+
+#[derive(Debug, Error)]
+#[error("MCPToolError: {0}")]
+pub struct MCPToolError(String);
+
+impl From<&rmcp::model::Tool> for ToolDefinition {
+    fn from(value: &rmcp::model::Tool) -> Self {
+        let name = value.name.to_string();
+        let description = value
+            .to_owned()
+            .description
+            .unwrap_or(name.clone().into())
+            .to_string();
+        let parameters = serde_json::Value::Object(value.input_schema.deref().to_owned());
+
+        Self {
+            name,
+            description,
+            parameters,
+        }
+    }
+}
+
+impl From<MCPToolError> for ToolError {
+    fn from(value: MCPToolError) -> Self {
+        Self::ToolCallError(Box::new(value))
     }
 }
