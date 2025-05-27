3c0b9c1a5bd50958b2cfdf9a7bf713f496d311cf
Kye Gomez <kye@swarms.world>
2025-05-26 23:59:27 -0700

    feat -- swarms api client

--- swarms-rs/src/structs/mod.rs
+++ swarms-rs/src/structs/mod.rs
@@ -6,9 +6,9 @@ pub mod graph_workflow;
 pub mod persistence;
 pub mod sequential_workflow;
 pub mod swarm;
+pub mod swarms_client;
 pub mod swarms_router;
 pub mod tool;
-
 mod utils;
 
 #[cfg(test)]
